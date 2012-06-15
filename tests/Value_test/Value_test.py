9e27965769335851ab8bd520e784439d602f8b67
Andrea Jokisaari <anmida@umich.edu>
2012-06-15 13:01:22 +0000

    changed exodiff tolerances on Value_test tests
    r11248

--- tests/Value_test/Value_test.py
+++ tests/Value_test/Value_test.py
@@ -4,10 +4,12 @@ Value_test = { INPUT : 'Value_test.i',
          EXODIFF : ['testValue.e']}
 
 ValueForcing_test = { INPUT : 'ValueForcing_test.i',
-         EXODIFF : ['testValueForcing.e']}
+         EXODIFF : ['testValueForcing.e'],
+         ABS_ZERO: 1e-8}
 
 ValueNucleation_test = { INPUT : 'ValueNucleation_test.i',
-         EXODIFF : ['testValueNucleation.e']}
+         EXODIFF : ['testValueNucleation.e'],
+         ABS_ZERO: 1e-8}
 
 ValueNucleationForcing_test = { INPUT : 'ValueNucleationForcing_test.i',
          EXODIFF : ['testValueNucleationForcing.e']}
