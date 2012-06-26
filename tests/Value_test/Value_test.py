cbe384fd469e6e1d9f23becbe874f752b5c79e9c
Cody Permann <cody.permann@inl.gov>
2012-06-26 14:52:30 +0000

    Disabling tests on Petsc 2.x
    r11493

--- tests/Value_test/Value_test.py
+++ tests/Value_test/Value_test.py
@@ -1,18 +1,23 @@
 from options import *
 
 Value_test = { INPUT : 'Value_test.i',
-         EXODIFF : ['testValue.e']}
+         EXODIFF : ['testValue.e'],
+         PETSC_VERSION : ['3.1.0']}
 
 ValueForcing_test = { INPUT : 'ValueForcing_test.i',
          EXODIFF : ['testValueForcing.e'],
+         PETSC_VERSION : ['3.1.0'],
          ABS_ZERO: 1e-8}
 
 ValueNucleation_test = { INPUT : 'ValueNucleation_test.i',
          EXODIFF : ['testValueNucleation.e'],
+         PETSC_VERSION : ['3.1.0'],
          ABS_ZERO: 1e-8}
 
 ValueNucleationForcing_test = { INPUT : 'ValueNucleationForcing_test.i',
-         EXODIFF : ['testValueNucleationForcing.e']}
+         EXODIFF : ['testValueNucleationForcing.e'],
+         PETSC_VERSION : ['3.1.0']}
 
 ACNucleationForcing_test = { INPUT : 'ACNucleationForcing_test.i',
-         EXODIFF : ['testACNucleationForcing.e']}
+         EXODIFF : ['testACNucleationForcing.e'],
+         PETSC_VERSION : ['3.1.0']}
