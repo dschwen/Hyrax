2de060ba5a74a796c5a44a6b807cb9153b52b42d
Cody Permann <cody.permann@inl.gov>
2012-10-18 13:40:16 +0000

    Skipping Tests on Petsc2.x (Zero Pivot issues)
    r14352

--- tests/nucleation_auxkernels_test/nucleation_auxkernels_test.py
+++ tests/nucleation_auxkernels_test/nucleation_auxkernels_test.py
@@ -1,8 +1,10 @@
 from options import *
 
 AuxNucleationRate_refine0_test_test = { INPUT : 'AuxNucleationRate_refine0.i',
-         EXODIFF : ['AuxNucleationRate_refine0.e']}
+         EXODIFF : ['AuxNucleationRate_refine0.e'],
+         PETSC_VERSION : ['>=3.1.0']}
 
 
 AuxNucleationRate_refine1_test_test = { INPUT : 'AuxNucleationRate_refine1.i',
-         EXODIFF : ['AuxNucleationRate_refine1.e']}
+         EXODIFF : ['AuxNucleationRate_refine1.e'],
+         PETSC_VERSION : ['>=3.1.0']}
