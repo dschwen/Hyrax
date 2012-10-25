e19653c1430e189e7ae1dfdd8d047e3b5c845b07
Andrea Jokisaari <anmida@umich.edu>
2012-10-25 06:43:35 +0000

    made private variables protected in CHBulkCoupled
    r14585

--- include/kernels/CHBulkCoupled.h
+++ include/kernels/CHBulkCoupled.h
@@ -39,7 +39,7 @@ protected:
 
   virtual RealGradient computeGradDFDCons(PFFunctionType type, Real c, RealGradient grad_c);
 
-private:
+
 
   MaterialProperty<Real> & _a1;  ///< Landau polynomial parameters (see Guo, 2008)
   MaterialProperty<Real> & _a2;
@@ -47,6 +47,8 @@ private:
 
   VariableValue & _coupled_OP_var;  ///< Allen-Cahn equation variable (order parameter, probably)
   VariableGradient & _coupled_OP_grad;  ///< gradient of AC variable
+
+  private:
 };
 
 #endif //CHBULKCOUPLED_H
