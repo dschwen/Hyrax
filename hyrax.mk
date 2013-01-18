088cedb0d887e90de93cc00d963d651e1b820055
John Peterson <jw.peterson@inl.gov>
2013-01-18 11:46:07 +0000

    Updating apps to use automake libmesh.
    r15945

--- hyrax.mk
+++ hyrax.mk
@@ -5,7 +5,7 @@ hyrax_INCLUDE  := $(foreach i, $(hyrax_INC_DIRS), -I$(i))
 
 libmesh_INCLUDE := $(hyrax_INCLUDE) $(libmesh_INCLUDE)
 
-hyrax_LIB := $(HYRAX_DIR)/libhyrax-$(METHOD)$(libext)
+hyrax_LIB := $(HYRAX_DIR)/libhyrax-$(METHOD).la
 LIBS += $(hyrax_LIB)
 
 hyrax_APP := $(HYRAX_DIR)/hyrax-$(METHOD)
@@ -15,13 +15,12 @@ hyrax_srcfiles    := $(shell find $(hyrax_SRC_DIRS) -name *.C)
 hyrax_csrcfiles   := $(shell find $(hyrax_SRC_DIRS) -name *.c)
 hyrax_fsrcfiles   := $(shell find $(hyrax_SRC_DIRS) -name *.f)
 hyrax_f90srcfiles := $(shell find $(hyrax_SRC_DIRS) -name *.f90)
+
 # object files
 hyrax_objects	:= $(patsubst %.C, %.$(obj-suffix), $(hyrax_srcfiles))
 hyrax_objects	+= $(patsubst %.c, %.$(obj-suffix), $(hyrax_csrcfiles))
 hyrax_objects += $(patsubst %.f, %.$(obj-suffix), $(hyrax_fsrcfiles))
 hyrax_objects += $(patsubst %.f90, %.$(obj-suffix), $(hyrax_f90srcfiles))
-
-hyrax_app_objects := $(patsubst %.C, %.$(obj-suffix), $(HYRAX_DIR)/src/main.C)
 
 # plugin files
 hyrax_plugfiles   := $(shell find $(HYRAX_DIR)/plugins/ -name *.C 2>/dev/null)
@@ -35,29 +34,32 @@ hyrax_plugins     += $(patsubst %.c, %-$(METHOD).plugin, $(hyrax_cplugfiles))
 hyrax_plugins     += $(patsubst %.f, %-$(METHOD).plugin, $(hyrax_fplugfiles))
 hyrax_plugins     += $(patsubst %.f90, %-$(METHOD).plugin, $(hyrax_f90plugfiles))
 
+# hyrax main
+hyrax_main_src    := $(HYRAX_DIR)/src/main.C
+hyrax_app_objects := $(patsubst %.C, %.$(obj-suffix), $(hyrax_main_src))
+
+# dependency files
+hyrax_deps := $(patsubst %.C, %.$(obj-suffix).d, $(hyrax_srcfiles)) \
+              $(patsubst %.c, %.$(obj-suffix).d, $(hyrax_csrcfiles)) \
+              $(patsubst %.C, %.$(obj-suffix).d, $(hyrax_main_src))
+
+# If building shared libs, make the plugins a dependency, otherwise don't.
+ifeq ($(libmesh_shared),yes)
+  hyrax_plugin_deps := $(hyrax_plugins)
+else
+  hyrax_plugin_deps :=
+endif
+
 all:: $(hyrax_LIB)
 
-# build rule for lib HYRAX
-ifeq ($(enable-shared),yes)
-# Build dynamic library
-$(hyrax_LIB): $(hyrax_objects) $(hyrax_plugins)
+$(hyrax_LIB): $(hyrax_objects) $(hyrax_plugin_deps)
 	@echo "Linking "$@"..."
-	@$(libmesh_CC) $(libmesh_CXXSHAREDFLAG) -o $@ $(hyrax_objects) $(libmesh_LDFLAGS)
-else
-# Build static library
-ifeq ($(findstring darwin,$(hostos)),darwin)
-$(hyrax_LIB): $(hyrax_objects)
-	@echo "Linking "$@"..."
-	@libtool -static -o $@ $(hyrax_objects)
-else
-$(hyrax_LIB): $(hyrax_objects)
-	@echo "Linking "$@"..."
-	@$(AR) rv $@ $(hyrax_objects)
-endif
-endif
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
+	  $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(hyrax_objects) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(EXTERNAL_FLAGS) -rpath $(HYRAX_DIR)
+	@$(libmesh_LIBTOOL) --mode=install --quiet install -c $(hyrax_LIB) $(HYRAX_DIR)
 
 # include HYRAX dep files
--include $(HYRAX_DIR)/src/*/*.d
+-include $(hyrax_deps)
 
 
 # how to build HYRAX application
@@ -68,15 +70,15 @@ hyrax: $(hyrax_APP)
 
 $(hyrax_APP): $(moose_LIB) $(elk_MODULES) $(hyrax_LIB) $(hyrax_app_objects)
 	@echo "Linking "$@"..."
-	@$(libmesh_CXX) $(libmesh_CXXFLAGS) $(hyrax_app_objects) -o $@ $(hyrax_LIB) $(elk_MODULES) $(moose_LIB) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(ADDITIONAL_LIBS)
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
+          $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(hyrax_app_objects) $(hyrax_LIB) $(elk_MODULES) $(moose_LIB) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(ADDITIONAL_LIBS)
 
--include $(HYRAX_DIR)/src/*.d
 endif
 
 #
 # Maintenance
 #
-delete_list := $(hyrax_APP) $(hyrax_LIB)
+delete_list := $(hyrax_APP) $(hyrax_LIB) $(HYRAX_DIR)/libhyrax-$(METHOD).*
 
 cleanall:: 
 	make -C $(HYRAX_DIR) clean 
