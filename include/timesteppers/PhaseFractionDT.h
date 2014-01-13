39ef137071695c65a6d5a052772e666289050d25
Andrew Slaughter <andrew.slaughter@inl.gov>
2014-01-13 17:48:07 +0000

    Revert "Reporter removed; PostprocessorInterface now returns const references (refs #2318)"
    This reverts commit 0006728dacf687725bc9b4e56ad6f063654a1fef.
    
    r23896

--- include/timesteppers/PhaseFractionDT.h
+++ include/timesteppers/PhaseFractionDT.h
@@ -38,8 +38,8 @@ protected:
 
   Real _growth_factor;
 
-  const PostprocessorValue & _volfrac;
-  const PostprocessorValue & _volfrac_old;
+  PostprocessorValue & _volfrac;
+  PostprocessorValue & _volfrac_old;
 
 private:
 };
