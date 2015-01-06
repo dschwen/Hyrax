idaholab/raven

Revert "Makefile updates for all applications"
This reverts commit 6c0a07f68a69ea7e1c86c16c3615f1518c617886.

r24898

Makefile
@@ -13,7 +13,8 @@
 # Note: Make sure that there is no whitespace after the word 'yes' if enabling
 # an application
 ###############################################################################
-ROOT_DIR        ?= $(shell dirname `pwd`)
+CURR_DIR    ?= $(shell pwd)
+ROOT_DIR    ?= $(shell dirname `pwd`)
 
 ifeq ($(MOOSE_DEV),true)
 	MOOSE_DIR ?= $(ROOT_DIR)/devel/moose
@@ -21,24 +22,25 @@ else
 	MOOSE_DIR ?= $(ROOT_DIR)/moose
 endif
 
+ELK_DIR     ?= $(ROOT_DIR)/elk
+R7_DIR     ?= $(ROOT_DIR)/r7_moose
+RAVEN_DIR   ?= $(ROOT_DIR)/raven
+
+APPLICATION_NAME := RAVEN
+
+DEP_APPS    ?= $(shell $(MOOSE_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
+#ADDITIONAL_LIBS  += -lgsl
+
 ################################## ELK MODULES ################################
 ALL_ELK_MODULES := yes
 ###############################################################################
 
-# framework
+
 include $(MOOSE_DIR)/build.mk
+# deps
 include $(MOOSE_DIR)/moose.mk
-
-# modules
-ELK_DIR ?= $(ROOT_DIR)/elk
 include $(ELK_DIR)/elk.mk
-
-# dep apps
-APPLICATION_DIR    := $(ROOT_DIR)/r7_moose
-APPLICATION_NAME   := r7_moose
-DEP_APPS           := $(shell $(MOOSE_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
-include            $(MOOSE_DIR)/app.mk
-
+include $(R7_DIR)/r7.mk
 include $(RAVEN_DIR)/config.mk
 include $(RAVEN_DIR)/raven.mk
 include $(RAVEN_DIR)/raven_python_modules.mk

dschwen/Hyrax

Makefile
@@ -12,6 +12,7 @@
 # Note: Make sure that there is no whitespace after the word 'yes' if enabling
 # an application
 ###############################################################################
+CURR_DIR	?= $(shell pwd)
 ROOT_DIR        ?= $(shell dirname `pwd`)
 
 ifeq ($(MOOSE_DEV),true)
@@ -19,6 +20,13 @@ ifeq ($(MOOSE_DEV),true)
 else
 	MOOSE_DIR ?= $(ROOT_DIR)/moose
 endif
+ELK_DIR         ?= $(ROOT_DIR)/elk
+HYRAX_DIR       ?= $(ROOT_DIR)/hyrax
+
+MAKE_LIBRARY := no
+APPLICATION_NAME := hyrax
+
+DEP_APPS    ?= $(shell $(MOOSE_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
 
 ################################## ELK MODULES ################################
 PHASE_FIELD       := yes
@@ -28,20 +36,11 @@ HEAT_CONDUCTION   := yes
 MISC              := yes
 ###############################################################################
 
-# framework
 include $(MOOSE_DIR)/build.mk
+#dependencies
 include $(MOOSE_DIR)/moose.mk
-
-# modules
-ELK_DIR ?= $(ROOT_DIR)/elk
 include $(ELK_DIR)/elk.mk
-
-# dep apps
-APPLICATION_DIR    := $(ROOT_DIR)/hyrax
-APPLICATION_NAME   := hyrax
-BUILD_EXEC         := yes
-DEP_APPS           := $(shell $(MOOSE_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
-include            $(MOOSE_DIR)/app.mk
+include $(HYRAX_DIR)/hyrax.mk
 
 ###############################################################################
 # Additional special case targets should be added here