idaholab/moose

Application Refactoring -refs #2021
r20262

test/src/main.C
@@ -1,5 +1,4 @@
 #include "MooseInit.h"
-#include "MooseTest.h"
 #include "MooseTestApp.h"
 #include "Moose.h"
 #include "AppFactory.h"
@@ -13,7 +12,7 @@ int main(int argc, char *argv[])
   MooseInit init(argc, argv);
 
   // Register this application's MooseApp and any it depends on
-  MooseTest::registerApps();
+  MooseTestApp::registerApps();
 
   // This creates dynamic memory that we're responsible for deleting
   MooseApp * app = AppFactory::createApp("MooseTestApp", argc, argv);

dschwen/Hyrax

src/main.C
@@ -12,7 +12,7 @@
 *  system.
 *************************************************************************/
 
-#include "Hyrax.h"
+#include "HyraxApp.h"
 //Moose Includes
 #include "MooseInit.h"
 #include "Moose.h"
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
   MooseInit init(argc, argv);
 
   // Register this application's MooseApp and any it depends on
-  Hyrax::registerApps();
+  HyraxApp::registerApps();
 
   // This creates dynamic memory that we're responsible for deleting
   MooseApp * app = AppFactory::createApp("HyraxApp", argc, argv);